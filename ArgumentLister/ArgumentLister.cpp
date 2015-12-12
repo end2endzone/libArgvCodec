b1409662f74335ef56be4ab01cbd70948beab55e
antoine <antoine@c820c9e9-158e-5f4c-997c-4e918743ed38>
2015-12-12 14:59:48 +0000

    - Implemented new features of ArgumentManager - Added ArgumentLister
    git-svn-id: http://localhost/svn/libargvcodec/trunk@6 c820c9e9-158e-5f4c-997c-4e918743ed38

---
+++ ArgumentLister/ArgumentLister.cpp
@@ -0,0 +1,25 @@
+// ArgumentLister.cpp : Defines the entry point for the console application.
+//
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+//int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, char* argv[])
+{
+  FILE * f = fopen("ArgumentLister.log", "w");
+  if (!f)
+    return 1;
+
+  for(int i=0; i<argc; i++)
+  {
+    //printf(   "arg%d=%s\n", i, argv[i]);
+    fprintf(f, "arg%d=%s\n", i, argv[i]);
+  }
+
+  fclose(f);
+	return 0;
+}
