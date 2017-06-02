12c7caefa96bfea673617b53366be9613a2d7a55
antoine <antoine@c820c9e9-158e-5f4c-997c-4e918743ed38>
2017-06-02 23:54:46 +0000

    - Removed code from utils.* which was already supported by gTestHelper.
    git-svn-id: http://localhost/svn/libargvcodec/trunk@50 c820c9e9-158e-5f4c-997c-4e918743ed38

--- libargvcodec/utils.h
+++ libargvcodec/utils.h
@@ -10,10 +10,9 @@ namespace utils
 
 void strReplace(std::string & iValue, const std::string & iOldStr, const std::string & iNewStr);
 void strReplace(std::string & iValue, const char * iOldStr, const char * iNewStr);
+size_t findNumTrailingBackslashes(const char * iValue);
 std::string uppercase(const std::string & iValue);
 std::string uppercase(const char * iValue);
-
-size_t findNumTrailingBackslashes(const char * iValue);
 
 }; //namespace utils
 }; //namespace libargvcodec
