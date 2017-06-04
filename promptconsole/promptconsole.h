c2401ce25849b9274a853e8ed2037d60ae7ac525
antoine <antoine@c820c9e9-158e-5f4c-997c-4e918743ed38>
2017-06-04 20:23:30 +0000

    - Created promptconsole library to handle "Press a key to continue..." issue when launching a program from file explorer. - Modified argencoder and showargs applications to use the API to wait for a key before closing allowing the user to see the program output before it closes.
    git-svn-id: http://localhost/svn/libargvcodec/trunk@64 c820c9e9-158e-5f4c-997c-4e918743ed38

---
+++ promptconsole/promptconsole.h
@@ -0,0 +1,4 @@
+#pragma once
+
+bool isPromptConsoleWindow();
+bool hasConsoleOwnership();
--- showargs/showargs.cpp
+++ showargs/showargs.cpp
@@ -6,6 +6,9 @@
 #include <stdio.h>
 #include <tchar.h>
 #include <iostream>
+#include <conio.h>
+
+#include "promptconsole.h"
 
 using namespace std;
 
@@ -16,5 +19,14 @@ int main(int argc, char* argv[])
     const char * value = argv[i];
     cout << "arg" << i << ": " << value << endl;
   }
+
+  //is this application that spawned a console?
+  //if so, then we ask the user to press a key before it goes away
+  if (hasConsoleOwnership())
+  {
+    cout << "Press a key to terminate application..." << endl;
+    _getch();
+  }
+
 	return 0;
 }
