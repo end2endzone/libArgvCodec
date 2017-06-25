d753294bfc198a63fcc27d89f95fb40619b8bee7
antoine <antoine@c820c9e9-158e-5f4c-997c-4e918743ed38>
2017-06-25 14:47:41 +0000

    *** Merging revisions 82-86 of http://localhost/svn/libargvcodec/branches/appsrename into F:\Projets\Programmation\C++\libargvcodec\trunk, respecting ancestry
    git-svn-id: http://localhost/svn/libargvcodec/trunk@95 c820c9e9-158e-5f4c-997c-4e918743ed38

--- copyright_install.bat
+++ copyright_install.bat
@@ -1,47 +1,47 @@
 @echo off
 
-call :INSERT %~dp0argencoder\main.cpp
-call :INSERT %~dp0argencoder\targetver.h
+call :INSERT %~dp0ArgEncoder\main.cpp
+call :INSERT %~dp0ArgEncoder\targetver.h
 
-call :INSERT %~dp0libargvcodec\ArgumentList.cpp
-call :INSERT %~dp0libargvcodec\ArgumentList.h
-call :INSERT %~dp0libargvcodec\CmdPromptArgumentCodec.cpp
-call :INSERT %~dp0libargvcodec\CmdPromptArgumentCodec.h
-call :INSERT %~dp0libargvcodec\CreateProcessArgumentCodec.cpp
-call :INSERT %~dp0libargvcodec\CreateProcessArgumentCodec.h
-call :INSERT %~dp0libargvcodec\dllmain.cpp
-call :INSERT %~dp0libargvcodec\IArgumentDecoder.h
-call :INSERT %~dp0libargvcodec\IArgumentEncoder.h
-call :INSERT %~dp0libargvcodec\libargvcodec.h
-call :INSERT %~dp0libargvcodec\macros.h
-call :INSERT %~dp0libargvcodec\os.cpp
-call :INSERT %~dp0libargvcodec\os.h
-call :INSERT %~dp0libargvcodec\targetver.h
-call :INSERT %~dp0libargvcodec\utils.cpp
-call :INSERT %~dp0libargvcodec\utils.h
+call :INSERT %~dp0libArgvCodec\ArgumentList.cpp
+call :INSERT %~dp0libArgvCodec\ArgumentList.h
+call :INSERT %~dp0libArgvCodec\CmdPromptArgumentCodec.cpp
+call :INSERT %~dp0libArgvCodec\CmdPromptArgumentCodec.h
+call :INSERT %~dp0libArgvCodec\CreateProcessArgumentCodec.cpp
+call :INSERT %~dp0libArgvCodec\CreateProcessArgumentCodec.h
+call :INSERT %~dp0libArgvCodec\dllmain.cpp
+call :INSERT %~dp0libArgvCodec\IArgumentDecoder.h
+call :INSERT %~dp0libArgvCodec\IArgumentEncoder.h
+call :INSERT %~dp0libArgvCodec\libArgvCodec.h
+call :INSERT %~dp0libArgvCodec\macros.h
+call :INSERT %~dp0libArgvCodec\os.cpp
+call :INSERT %~dp0libArgvCodec\os.h
+call :INSERT %~dp0libArgvCodec\targetver.h
+call :INSERT %~dp0libArgvCodec\utils.cpp
+call :INSERT %~dp0libArgvCodec\utils.h
 
-call :INSERT %~dp0libargvcodectest\ArgumentLister.cpp
-call :INSERT %~dp0libargvcodectest\ArgumentLister.h
-call :INSERT %~dp0libargvcodectest\gtesthelper.cpp
-call :INSERT %~dp0libargvcodectest\gtesthelper.h
-call :INSERT %~dp0libargvcodectest\main.cpp
-call :INSERT %~dp0libargvcodectest\targetver.h
-call :INSERT %~dp0libargvcodectest\TestArgumentList.cpp
-call :INSERT %~dp0libargvcodectest\TestArgumentList.h
-call :INSERT %~dp0libargvcodectest\TestCmdPromptArgumentCodec.cpp
-call :INSERT %~dp0libargvcodectest\TestCmdPromptArgumentCodec.h
-call :INSERT %~dp0libargvcodectest\TestCreateProcessArgumentCodec.cpp
-call :INSERT %~dp0libargvcodectest\TestCreateProcessArgumentCodec.h
-call :INSERT %~dp0libargvcodectest\TestUtils.cpp
-call :INSERT %~dp0libargvcodectest\TestUtils.h
-call :INSERT %~dp0libargvcodectest\TestValidator.cpp
-call :INSERT %~dp0libargvcodectest\TestValidator.h
+call :INSERT %~dp0libArgvCodecTest\ArgumentLister.cpp
+call :INSERT %~dp0libArgvCodecTest\ArgumentLister.h
+call :INSERT %~dp0libArgvCodecTest\gtesthelper.cpp
+call :INSERT %~dp0libArgvCodecTest\gtesthelper.h
+call :INSERT %~dp0libArgvCodecTest\main.cpp
+call :INSERT %~dp0libArgvCodecTest\targetver.h
+call :INSERT %~dp0libArgvCodecTest\TestArgumentList.cpp
+call :INSERT %~dp0libArgvCodecTest\TestArgumentList.h
+call :INSERT %~dp0libArgvCodecTest\TestCmdPromptArgumentCodec.cpp
+call :INSERT %~dp0libArgvCodecTest\TestCmdPromptArgumentCodec.h
+call :INSERT %~dp0libArgvCodecTest\TestCreateProcessArgumentCodec.cpp
+call :INSERT %~dp0libArgvCodecTest\TestCreateProcessArgumentCodec.h
+call :INSERT %~dp0libArgvCodecTest\TestUtils.cpp
+call :INSERT %~dp0libArgvCodecTest\TestUtils.h
+call :INSERT %~dp0libArgvCodecTest\TestValidator.cpp
+call :INSERT %~dp0libArgvCodecTest\TestValidator.h
 
 call :INSERT %~dp0libPromptConsole\libPromptConsole.cpp
 call :INSERT %~dp0libPromptConsole\libPromptConsole.h
 
-call :INSERT %~dp0showargs\showargs.cpp
-call :INSERT %~dp0showargs\targetver.h
+call :INSERT %~dp0ShowArgs\ShowArgs.cpp
+call :INSERT %~dp0ShowArgs\targetver.h
 
 pause
 
