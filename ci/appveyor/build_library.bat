77dd1ad2b897fa640f8aab81fd3c3fb66bfe5114
Antoine <end2endzone@gmail.com>
2018-10-09 22:35:21 -0400

    Fixed an issue with an incorrect build option parameter

--- ci/appveyor/build_library.bat
+++ ci/appveyor/build_library.bat
@@ -16,7 +16,7 @@ echo ===========================================================================
 cd /d %APPVEYOR_BUILD_FOLDER%
 mkdir build >NUL 2>NUL
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=%INSTALL_LOCATION% -DNUMBERTOWORDS_BUILD_TEST=ON ..
+cmake -DCMAKE_INSTALL_PREFIX=%INSTALL_LOCATION% -DLIBARGVCODEC_BUILD_TEST=ON ..
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo ============================================================================
